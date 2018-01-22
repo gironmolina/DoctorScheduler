5067dce6a8c292f25faf706710e05a997f4416d5
Juan Giron <gironmolina@gmail.com>
2018-01-22 18:15:09 +0100

    changes

---
+++ DoctorScheduler/DoctorScheduler.Web/src/app/scheduler/scheduler.ts
@@ -0,0 +1,34 @@
+export interface IScheduler{
+    Facility: IFacility,
+    SlotDurationMinutes: number,
+    Monday: ISlot,
+    Tuesday: ISlot,
+    Wednesday: ISlot,
+    Thursday: ISlot,    
+    Friday: ISlot,
+    Saturday: ISlot,
+    Sunday: ISlot
+}
+
+export interface IFacility {
+    FacilityId: string;
+    Name: string;
+    Address: string;
+}
+
+export interface ISlot {
+    WorkPeriod: IWorkPeriod;
+    BusySlots: IBusySlots;
+}
+
+export interface IWorkPeriod {
+    StartHour: number;
+    EndHour: number;
+    LunchStartHour: number;
+    LunchEndHour: number;
+}
+
+export interface IBusySlots {
+    Start: string;
+    End: string;
+}
