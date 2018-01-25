32d2c26bb962c23ddb23f9d4c6837f4453967157
Juan Giron <gironmolina@gmail.com>
2018-01-25 17:07:52 +0100

    Latest changes

--- DoctorScheduler/DoctorScheduler.Web/src/app/scheduler/scheduler.component.ts
+++ DoctorScheduler/DoctorScheduler.Web/src/app/scheduler/scheduler.component.ts
@@ -16,30 +16,54 @@ export class SchedulerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this._schedulerService.getSchedulers("20180101")
+    var mondayDate = this.getMonday(new Date());
+    var dateFormat = this.convertDate(mondayDate);
+    this.getScheduler(dateFormat);
+  }
+
+  private getScheduler(date : string): void {
+    this._schedulerService.getSchedulers(date)
             .subscribe(scheduler => { this.scheduler = scheduler                    
             },
             error => this.errorMessage = <any>error);
   }
 
-  previousDate(): void {
+  private previousDate(): void {
     console.log("previousDate");
   }
 
-  nextDate(): void {
-      console.log("nextDate");
-      var d = new Date();
-      d.setDate(d.getDate() + (1 + 7 - d.getDay()) % 7);
-      console.log(d);
+  private nextDate(): void {
+    var currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + (1 + 7 - currentDate.getDay()) % 7);
+    var qqq = this.convertDate(currentDate);
+    console.log(qqq);
+  }
+
+  getMonday(date: Date): Date {
+    var currentDate = new Date(date);
+    var dayOfTheWeek = currentDate.getDay();
+    var mondayDay = currentDate.getDate() - dayOfTheWeek + (dayOfTheWeek == 0 ? -6 : 1);
+    return new Date(currentDate.setDate(mondayDay));
   }
 
   getNextDayOfWeek(date, dayOfWeek): string {
     // Code to check that date and dayOfWeek are valid left as an exercise ;)
-
     var resultDate = new Date(date.getTime());
-
+    var asd = date.getDate();
     resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+
+    var qqq = this.convertDate(resultDate);
 
     return resultDate.toString();
   }
+
+  convertDate(asd : Date): string {
+    var mm = asd.getMonth() + 1; // getMonth() is zero-based
+    var dd = asd.getDate();
+  
+    return [asd.getFullYear(),
+            (mm>9 ? '' : '0') + mm,
+            (dd>9 ? '' : '0') + dd
+           ].join('');
+  };
 }
